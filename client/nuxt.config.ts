5d8127e8b2f64c3ae8f4e13f299406f1c5531bcf
Stanislav Snisar <sn.stanislav4@gmail.com>
2025-10-30 20:14:47 +0100

    Change on the client side the request path on server from localhost to production

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -15,7 +15,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     apiSecret: "",
     public: {
-      apiBase: "https://moviestat.vercel.app"
+      apiBase: "https://moviestat-0sul.onrender.com"
+      // http://localhost:3001/
     }
   },
   compatibilityDate: '2025-07-15',
