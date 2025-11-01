192ca5148a36e6dedd2a96cae234518670aefd00
Stanislav Snisar <sn.stanislav4@gmail.com>
2025-11-01 11:13:23 +0100

    Fix UI responsiveness

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -6,6 +6,13 @@ import { dirname, join } from "path";
 const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  },
   css: [join(currentDir, './assets/styles/main.css')],
   //css: ['~/assets/styles/main.scss'],
   vite: {
@@ -16,7 +23,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     apiSecret: "",
     public: {
-      apiBase: process.env.NODE_ENV === "production" ? "https://moviestat-0sul.onrender.com": "http://localhost:3001"
+      apiBase: process.env.NODE_ENV === "production" ? "https://moviestat-0sul.onrender.com" : "http://localhost:3001"
     }
   },
   compatibilityDate: '2025-07-15',
